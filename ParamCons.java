116af02bd7b1448b77f65f9af4b67f33e55eb8ea
pokurulokesh <97870411+pokurulokesh@users.noreply.github.com>
2022-01-17 17:44:59 +0530

    Add files via upload

---
+++ ParamCons.java
@@ -0,0 +1,12 @@
+package pokuru_lokesh1;
+
+		public class ParamCons {
+		public static void main(String[] args) {
+
+			Std std1=new Std(2,"Alex");
+			Std std2=new Std(10,"Annie");
+			std1.display();
+			std2.display();
+				}
+		}
+
